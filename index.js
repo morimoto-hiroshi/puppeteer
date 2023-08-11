57d887bd696029d0c2155f12a9a0f769588646ff
Hiroshi MORIMOTO <hiroshi.morimoto@metamoji.com>
2023-08-11 14:16:01 +0900

    取得urlを追加

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const puppeteer = require('puppeteer');
 
 (async() => {
+    await capture(`file://${process.cwd()}/sample.html`, 600, 800, 'sample');
+    await capture('https://techcrunch.com/2023/08/09/researchers-watched-100-hours-of-hackers-hacking-honeypot-computers/', 1280, 2560, 'sample2');
+})();
+
+async function capture(url, width, height, filename) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setViewport({width: 600, height: 800, deviceScaleFactor: 2});
-    console.log('ページ読み込み ..');
-    await page.goto(`file://${process.cwd()}/sample.html`, {waitUntil: 'networkidle2'});
+    await page.setViewport({width: width, height: height, deviceScaleFactor: 2});
+    console.log('ページ読み込み ..', `${width}x${height}`, url);
+    await page.goto(url, {waitUntil: 'networkidle2'});
     console.log('さらに2秒待機 ..');
     await sleep(2000);
     console.log('画像保存 ..');
-    await page.screenshot({path: './sample.jpg'});
+    await page.screenshot({path: `./${filename}.jpg`});
     console.log('PDF保存 ..');
-    await page.pdf({path: 'sample.pdf'});
+    await page.pdf({path: `${filename}.pdf`});
     browser.close();
     console.log('出力完了');
-})();
+}
 
 async function sleep(delay) {
     return new Promise(resolve => setTimeout(resolve, delay));
